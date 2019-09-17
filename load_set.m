43e2ff3fb22bf6495b6c698a7922974b65764d94
Yacine <yacine.mahdid@mail.mcgill.ca>
2019-09-16 21:11:11 -0400

    add basic loading .set

---
+++ load_set.m
@@ -0,0 +1,17 @@
+function [eeg,sampling_rate,number_channels,channels_location,channels_info] = load_set(file_name,path,type)
+%LOAD_EEG will load the EEG data
+%   file_name: name of the file to load
+%   path: path to that file
+%   type: type of the file to load (.set, edf)
+
+    % Currently supported format: .set files
+    if(strcmp(type,'set'))
+        data = pop_loadset(file_name,path);
+        eeg = data.data;
+        sampling_rate = data.srate;
+        number_channels = data.nbchan;
+        channels_location = data.chanlocs;
+        channels_info = data.chaninfo;
+    end
+end
+
