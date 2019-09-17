66a7c2ad0fa20a139d3db5f4bb91128a8d0e9d91
Bl0101 <yacine.mahdid@live.com>
2019-09-16 21:44:59 -0400

    add Recording class

--- load_set.m
+++ load_set.m
@@ -1,18 +1,13 @@
-function [Recording] = load_set(file_name,path)
+function [recording] = load_set(file_name,path)
 %LOAD_EEG will load the EEG data
 %   file_name: name of the file to load
 %   path: path to that file
 %
-%   Recording: structure containing the eeg data
+%   recording: instance of a Recording containing the eeg data
 
-    Recording = struct();
 
     % Currently supported format: .set files
     data = pop_loadset(file_name,path);
-    Recording.eeg = data.data;
-    Recording.sampling_rate = data.srate;
-    Recording.number_channels = data.nbchan;
-    Recording.channels_location = data.chanlocs;
-    Recording.channels_info = data.chaninfo;
+    recording = Recording(data.data, data.srate, data.nbchan, data.chanlocs);
 end
 
