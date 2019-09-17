fae504a6a7e27ec9fb2f88610631e5f5164ec22a
Bl0101 <yacine.mahdid@live.com>
2019-09-16 21:20:11 -0400

    refactor to simplify the returning structure

--- load_set.m
+++ load_set.m
@@ -1,17 +1,23 @@
-function [eeg,sampling_rate,number_channels,channels_location,channels_info] = load_set(file_name,path,type)
+function [EEG] = load_set(file_name,path)
 %LOAD_EEG will load the EEG data
 %   file_name: name of the file to load
 %   path: path to that file
-%   type: type of the file to load (.set, edf)
+
+    EEG = struct();
+    EEG.data = [];
+    EEG.sampling_rate = -1;
+    EEG.number_channels = -1;
+    EEG.channels_location = [];
+    EEG.channels_info = [];
 
     % Currently supported format: .set files
     if(strcmp(type,'set'))
         data = pop_loadset(file_name,path);
-        eeg = data.data;
-        sampling_rate = data.srate;
-        number_channels = data.nbchan;
-        channels_location = data.chanlocs;
-        channels_info = data.chaninfo;
+        EEG.data = data.data;
+        EEG.sampling_rate = data.srate;
+        EEG.number_channels = data.nbchan;
+        EEG.channels_location = data.chanlocs;
+        EEG.channels_info = data.chaninfo;
     end
 end
 
