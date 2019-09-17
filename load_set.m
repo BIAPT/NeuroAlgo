6d99e03605e0b104407d831d71fd5a485ca583ed
Bl0101 <yacine.mahdid@live.com>
2019-09-16 21:29:15 -0400

    remove unused files

--- load_set.m
+++ load_set.m
@@ -6,20 +6,13 @@ function [Recording] = load_set(file_name,path)
 %   Recording: structure containing the eeg data
 
     Recording = struct();
-    Recording.eeg = [];
-    Recording.sampling_rate = -1;
-    Recording.number_channels = -1;
-    Recording.channels_location = [];
-    Recording.channels_info = [];
 
     % Currently supported format: .set files
-    if(strcmp(type,'set'))
-        data = pop_loadset(file_name,path);
-        Recording.eeg = data.data;
-        Recording.sampling_rate = data.srate;
-        Recording.number_channels = data.nbchan;
-        Recording.channels_location = data.chanlocs;
-        Recording.channels_info = data.chaninfo;
-    end
+    data = pop_loadset(file_name,path);
+    Recording.eeg = data.data;
+    Recording.sampling_rate = data.srate;
+    Recording.number_channels = data.nbchan;
+    Recording.channels_location = data.chanlocs;
+    Recording.channels_info = data.chaninfo;
 end
 
