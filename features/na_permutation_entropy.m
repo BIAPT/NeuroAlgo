ed3e81f777c71f4ecf7028ee00268c0a271744d0
Bl0101 <yacine.mahdid@live.com>
2019-09-23 19:04:11 -0400

    add permutation entropy as a feature

---
+++ features/na_permutation_entropy.m
@@ -0,0 +1,33 @@
+function [result] = na_permutation_entropy(recording, frequency_band, window_size,embedding_dimension, time_lag ,is_verbose)
+    %NA_PERMUTATION_ENTROPY NeuroAlgo implementation of wpli that works with Recording
+    % NOTE: right now we are only doing non-overlapping window (in sec)
+    % NOTE: We are also only doing fullband eeg
+    result = Result('wpli', recording);
+    channels_location = recording.channels_location;
+    print(strcat("Filtering Data from ",string(frequency_band(1)), "Hz to ", string(frequency_band(2)), "Hz."),is_verbose);
+    filtered_data = recording.filter_data(recording.data, frequency_band);
+    windowed_data = recording.create_window(filtered_data, window_size);
+    [number_window,~,~] = size(windowed_data);
+    
+    anterior_mask = ([channels_location.is_anterior] == 1);
+    posterior_mask = ([channels_location.is_posterior] == 1);
+    
+    %% Calculation on the windowed segments
+    result.data.permutation_entropy = zeros(number_window, recording.number_channels);
+    result.data.normalized_permutation_entropy = zeros(number_window, recording.number_channels);
+    result.data.avg_permutation_entropy_anterior = zeros(1,number_window);
+    result.data.avg_permutation_entropy_posterior = zeros(1,number_window); 
+    
+    for i = 1:number_window
+       print(strcat("Permutation Entropy at window: ",string(i)," of ", string(number_window)),is_verbose); 
+       segment_data = squeeze(windowed_data(i,:,:));
+       [pe, normalized_pe] = permutation_entropy(segment_data, embedding_dimension, time_lag); 
+       result.data.permutation_entropy(i,:,:) = pe;
+       
+       % Saving the information
+       result.data.normalized_permutation_entropy(i,:,:) = normalized_pe;
+       result.data.avg_permutation_entropy_anterior(i) = mean(normalized_pe(anterior_mask));
+       result.data.avg_permutation_entropy_posterior(i) = mean(normalized_pe(posterior_mask));
+    end
+end
+
--- lib/eegsonic/permutation_entropy.m
+++ lib/eegsonic/permutation_entropy.m
@@ -1,59 +1,24 @@
-function [avg_pe_frontal,avg_pe_posterior] = permutation_entropy(eeg_data,eeg_info,parameters,frontal_mask,posterior_mask)
-%PERMUTATION_ENTROPY calculate the permutation entropy for the frontal and
-%posterior channels and return an average of both values
-%   Input:
-%       eeg_data: data to calculate the measures on
-%       eeg_info: headset information
-%       parameters: variables data as inputed by the user
-%       frontal_mask: boolean mask for the frontal electrodes
-%       posterior_mask: boolean mask for the posterior electrode
-%   Output:
-%       avg_pe_frontal: singular value representing pe of frontal
-%       electrodes
-%       avg_pe_posterior: singular value representing pe of posterior
-%       electrodes
-
-    %% Apply boolean mask
-    frontal_eeg = eeg_data(frontal_mask == 1,:)';
-    posterior_eeg = eeg_data(posterior_mask == 1,:)';
-
-    %% Extract parameters from data structure
-    embedding_dimension = parameters.embedding_dimension;
-    time_delay = parameters.time_delay;
-    
-    %% Calculate permutation entropy
-    % Here we use code from our collaborator (in lib/EEGsonic_function/features/helper
-    [~,frontal_pe] = permutation_entropy_mv(frontal_eeg,embedding_dimension,time_delay); 
-    [~,posterior_pe] = permutation_entropy_mv(posterior_eeg,embedding_dimension,time_delay);
-  
-    %% Average the values received
-    avg_pe_frontal = mean(frontal_pe);
-    avg_pe_posterior = mean(posterior_pe);
-end
-
-function [permutation_entropy, norm_permutation_entropy]=permutation_entropy_mv(data, embedding_dimension, time_lag)
+function [pe, normalized_pe] = permutation_entropy(eeg_data, embedding_dimension, time_lag)
 % permutation entropy for multivariate signals (just a repetition of calculation for multi-channel signals)
 % 2018.1.19. Heonsoo Lee
 % 2018.12.31 Yacine Mahdid (Refactored for readability)
-
-% Input:
-%   data: each column must be a time series
-%   embedding_dimension: embedding dimension, or pattern size
-%   time_lag: time lag
-%
-% Output:
-%   permutation_entropy: (unnormalized) permutation entropy
-%   norm_permutation_entropy: normalized permutation entropy
-
+%   Input:
+%       eeg_data: data to calculate the measures on
+%       embedding_dimension & time_delay: params for the pe code call (TODO
+%       find what these parameters are doing)
+%   Output:
+%       the normalized permutation entropy
+    
     %% Initialization of Variables
+    eeg_data = eeg_data';
     numpat=factorial(embedding_dimension); % number of patterns
     denom=log2(numpat); % normalization factor
-    Ddata=delay_reconstruction(data, time_lag, embedding_dimension); % delay reconstruction
-    [~,number_channels]=size(data); % get number of channels
+    Ddata=delay_reconstruction(eeg_data, time_lag, embedding_dimension); % delay reconstruction
+    [~,number_channels]=size(eeg_data); % get number of channels
    
     %% Pre-allocation of Output
-    permutation_entropy = zeros(1,number_channels);
-    norm_permutation_entropy = zeros(1,number_channels);
+    pe = zeros(1,number_channels);
+    normalized_pe = zeros(1,number_channels);
     
     for c=1:number_channels
         %% real value -> symoblic pattern
@@ -70,10 +35,11 @@ function [permutation_entropy, norm_permutation_entropy]=permutation_entropy_mv(
             ind=find(udata==u_patterns(u));
             p(u)=length(ind)/size(udata,1);
         end
-        permutation_entropy(1,c)=-sum(p.*log2(p+eps));
-        norm_permutation_entropy(1,c)=permutation_entropy(c)./denom;
+        pe(1,c)=-sum(p.*log2(p+eps));
+        normalized_pe(1,c)=pe(c)./denom;
         clear p;
     end
+  
 end
 
 function output=delay_reconstruction(data, delay_time, embedding_dimension)
