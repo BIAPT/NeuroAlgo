53748c082d20671bfd78fe69fcc1524c6d0f3bd7
Bl0101 <yacine.mahdid@live.com>
2019-09-23 18:44:07 -0400

    bundled the permutation entropy code within permutation_entropy.m
    It's most likely the only function that will use these other functions. Worst case if another function need the helper we know where to find them.

--- lib/eegsonic/permutation_entropy.m
+++ lib/eegsonic/permutation_entropy.m
@@ -31,3 +31,76 @@ function [avg_pe_frontal,avg_pe_posterior] = permutation_entropy(eeg_data,eeg_in
     avg_pe_posterior = mean(posterior_pe);
 end
 
+function [permutation_entropy, norm_permutation_entropy]=permutation_entropy_mv(data, embedding_dimension, time_lag)
+% permutation entropy for multivariate signals (just a repetition of calculation for multi-channel signals)
+% 2018.1.19. Heonsoo Lee
+% 2018.12.31 Yacine Mahdid (Refactored for readability)
+
+% Input:
+%   data: each column must be a time series
+%   embedding_dimension: embedding dimension, or pattern size
+%   time_lag: time lag
+%
+% Output:
+%   permutation_entropy: (unnormalized) permutation entropy
+%   norm_permutation_entropy: normalized permutation entropy
+
+    %% Initialization of Variables
+    numpat=factorial(embedding_dimension); % number of patterns
+    denom=log2(numpat); % normalization factor
+    Ddata=delay_reconstruction(data, time_lag, embedding_dimension); % delay reconstruction
+    [~,number_channels]=size(data); % get number of channels
+   
+    %% Pre-allocation of Output
+    permutation_entropy = zeros(1,number_channels);
+    norm_permutation_entropy = zeros(1,number_channels);
+    
+    for c=1:number_channels
+        %% real value -> symoblic pattern
+        udata=zeros(size(Ddata(:,:,c),1), 1);
+        [~, Ddata_sort]=sort(Ddata(:,:,c),2);
+        for i=1:embedding_dimension
+            udata=udata+Ddata_sort(:,i)*10^(round(embedding_dimension/2)-i)'; % *** double precision: 10^-16
+        end
+        %% probability
+        [u_patterns, ~]=unique(udata); % find unique patterns
+        u_len=length(u_patterns); % # of unique patterns
+        p=zeros(1, numpat);
+        for u=1:u_len
+            ind=find(udata==u_patterns(u));
+            p(u)=length(ind)/size(udata,1);
+        end
+        permutation_entropy(1,c)=-sum(p.*log2(p+eps));
+        norm_permutation_entropy(1,c)=permutation_entropy(c)./denom;
+        clear p;
+    end
+end
+
+function output=delay_reconstruction(data, delay_time, embedding_dimension)
+% INPUT
+% data: 2-d matrix (time by channel)
+% delay_time: delay time
+% embedding_dimension: embedding dimension
+%
+% OUTPUT: 3-d matrix (time by EMB vector by channel)
+% output(1,:,1) = [data(1) data(1+L) data(1+2L) ... data(1+(m-1)L)]
+%
+% Heonsoo Lee 2011.11.17
+% Yacine Mahdid 2018.12.31 (Refactored for readability)
+
+    %% Initialize variables
+    [length,number_channels]=size(data);
+    output=zeros(length-delay_time*(embedding_dimension-1),embedding_dimension,number_channels);
+
+    for current_channel=1:number_channels
+        for current_dimension=1:embedding_dimension
+            % Get the start and end times
+            start_time = 1+(current_dimension-1)*delay_time;
+            end_time = length-(embedding_dimension-current_dimension)*delay_time;
+            % slice the data
+            current_data = data(start_time:end_time, current_channel);
+            % store it at the right place
+            output(:, current_dimension, current_channel)= current_data;
+        end
+    end
+end
