01e1aab5c1b89a09521fab7631065000d2dd213a
Bl0101 <yacine.mahdid@live.com>
2019-10-07 07:25:33 -0400

    add modularity and binary small worldness

--- features/graph_theory/modularity.m
+++ features/graph_theory/modularity.m
@@ -1,7 +1,11 @@
-function [outputArg1,outputArg2] = modularity(inputArg1,inputArg2)
+function [community] = modularity(b_matrix, gamma)
 %MODULARITY Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%   b_matrix: a N*N binary square matrix
+%   null_networks: 3d matrix containing pre-made null_networks
+%   gamma: if large will detect smaller module, if small will detect larger
+%   module
+
+    [~,community] = community_louvain(b_matrix,gamma); % community, modularity
 end
+
 
