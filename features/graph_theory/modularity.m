167e10046dc69d215547b039788583efe7860073
Bl0101 <yacine.mahdid@live.com>
2019-10-10 19:43:39 -0400

    remove useless files

--- features/graph_theory/modularity.m
+++ features/graph_theory/modularity.m
@@ -1,11 +1,11 @@
-function [community] = modularity(b_matrix, gamma)
+function [community] = modularity(b_matrix)
 %MODULARITY Summary of this function goes here
 %   b_matrix: a N*N binary square matrix
 %   null_networks: 3d matrix containing pre-made null_networks
 %   gamma: if large will detect smaller module, if small will detect larger
 %   module
 
-    [~,community] = community_louvain(b_matrix,gamma); % community, modularity
+    [~,community] = community_louvain(b_matrix); % community, modularity
 end
 
 
