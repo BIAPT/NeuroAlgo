2096f5d4328405a15e4d9a6dc60c05316cac7704
Bl0101 <yacine.mahdid@live.com>
2019-10-15 23:29:02 -0400

    add test function for topo

--- example/example_analysis.m
+++ example/example_analysis.m
@@ -32,7 +32,7 @@ frequency_band = [7 13]; % This is in Hz
 window_size = 10; % This is in seconds and will be how we chunk the whole dataset
 number_surrogate = 10; % Number of surrogate wPLI to create
 p_value = 0.05; % the p value to make our test on
-step_size = 0.1;
+step_size = 10;
 result_wpli = na_wpli(recording, frequency_band, window_size, step_size, number_surrogate, p_value);
 
 % dPLI
