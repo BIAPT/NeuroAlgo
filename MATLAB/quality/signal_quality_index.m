ced0b3ffa6dc298fcbdaf8a129fd7af303454a04
Bl0101 <yacine.mahdid@live.com>
2019-11-16 11:33:58 -0500

    adds some more cleaning for the algorithm for SQI

--- MATLAB/quality/signal_quality_index.m
+++ MATLAB/quality/signal_quality_index.m
@@ -8,14 +8,16 @@ function [ sc_sqi, temp_sqi] = signal_quality_index(skin_conductance, temperatur
     % result consistency
     clear_internal;
     
-    %% 1.1 - Detrending filter
-    % Skin conductance signal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    skin_conductance = medfilt1(skin_conductance,75); %applymedian filter 
-    derr = diff(skin_conductance); %derivative correction
+    %% Filtering the signals
+    % Skin conductance signal
+    % Median filtering
+    skin_conductance = medfilt1(skin_conductance,75); 
+    % Derivative correction
+    derr = diff(skin_conductance); 
     z_der = zscore(derr);
     for k=11:length(derr)-10
-        
-        if z_der(k) >5 % corrects for steep positive slopes. 
+        % correct for steep positive slopes. 
+        if z_der(k) >5 
             derr(k)= median(derr(k-10:k+10));
         end
         
@@ -24,63 +26,63 @@ function [ sc_sqi, temp_sqi] = signal_quality_index(skin_conductance, temperatur
         if z_der(k)<-3 
            derr(k)= median(derr(k-10:k+10));
         end
-        
     end
     
-    filt_derr = cat(1,skin_conductance(1),derr);
-    data_in.sc = cumsum(filt_derr);
+    filt_derr = cat(1,skin_conductance(1), derr);
+    skin_conductance = cumsum(filt_derr);
 
-    % Skin Temperature signal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    data_in.skt = medfilt1(temperature,1);
+    % Skin Temperature signal 
+    temperature = medfilt1(temperature,1);
 
-    %set up placeholders for SQI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    sc_sqi= ones(1, length(data_in.sc));
-    sc_sqi=sc_sqi';
     
-    temp_sqi= ones(1, length(data_in.skt));
-    temp_sqi=temp_sqi';
+    %% Signal Quality Index Calculation
+    % Setup variables
+    sc_sqi = ones(length(skin_conductance),1);
+    temp_sqi = ones(length(temperature),1);
     
-    %% 2- SKIN CONDUCTANCE Signal Quality Index
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % SQI for Skin Conductance
     W = 30;
-    L = length(data_in.sc);  
+    L = length(skin_conductance);  
     for l1 = 1:15:L-W+1
         l2=l1-1+W;
-        dff= std(data_in.sc(l1:l2));
+        dff= std(skin_conductance(l1:l2));
         sc_sqi(l1:l2)=sc_sqi(l1:l2)*exp(-0.1*dff); 
-        if max(diff(data_in.sc(l1:l2))) > 3 
+        
+        if max(diff(skin_conductance(l1:l2))) > 3 
             sc_sqi(l1:l2) = 0.4*sc_sqi(l1:l2);
         end  
     end
+    
     %the below code must match any changes made above.
     if mod(L, 2)~=0
-       l1=L-W;
-       l2=L;
-       dff= std(data_in.sc(l1:l2));
-       sc_sqi(l1:l2)=sc_sqi(l1:l2)*exp(-0.1*dff);
-        if max(diff(data_in.sc(l1:l2))) > 3
+       l1 = L-W;
+       l2 = L;
+       dff = std(skin_conductance(l1:l2));
+       sc_sqi(l1:l2) = sc_sqi(l1:l2)*exp(-0.1*dff);
+        if max(diff(skin_conductance(l1:l2))) > 3
             sc_sqi(l1:l2) = 0.4*sc_sqi(l1:l2);
         end  
-
     end
 
     W = 120; %identifiy flat lines and decrease their signal quality
     for l1 = 1:30:L-W+1 
         l2=l1-1+W;
-            if max(abs(diff(data_in.sc(l1:l2)))) <= 0.001
-                sc_sqi(l1:l2) = 0.1*sc_sqi(l1:l2);
-            end 
+        if max(abs(diff(skin_conductance(l1:l2)))) <= 0.001
+            sc_sqi(l1:l2) = 0.1*sc_sqi(l1:l2);
+        end 
     end
 
-    for j=1:length(sc_sqi) %identify values out of normal range 
-        if data_in.sc(j) <= 0.02
+    for j = 1:length(sc_sqi) %identify values out of normal range 
+        if skin_conductance(j) <= 0.02
             sc_sqi(j) = 0;
         end
-         if data_in.sc(j) > 20
-            sc_sqi(j) = 0.65;
-         end
-         if data_in.sc(j) > 30
-            sc_sqi(j) = 0;
+        
+        if skin_conductance(j) > 20
+           sc_sqi(j) = 0.65;
+        end
+         
+        if skin_conductance(j) > 30
+           sc_sqi(j) = 0;
         end
     end
 
@@ -90,17 +92,17 @@ function [ sc_sqi, temp_sqi] = signal_quality_index(skin_conductance, temperatur
     %% 3- SKIN TEMPERATURE Signal Quality Index
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     W = 50;
-    L = length(data_in.skt);  
+    L = length(temperature);  
     for l1 = 1:25:L-W+1 %value betweeen 1 and L is half the window size   
         l2=l1-1+W;
-        dff= std(data_in.skt(l1:l2));
-        temp_min = min(data_in.skt(l1:l2));
-        temp_maxmin = max(data_in.skt(l1:l2))-min(data_in.skt(l1:l2));
+        dff= std(temperature(l1:l2));
+        temp_min = min(temperature(l1:l2));
+        temp_maxmin = max(temperature(l1:l2))-min(temperature(l1:l2));
         temp_sqi(l1:l2)=temp_sqi(l1:l2)*exp(-0.20*dff); %sensitivity to SD
         if temp_min < 15 %identify out of range values 
                 temp_sqi(l1:l2) = 0.50*temp_sqi(l1:l2); 
         end
-        if max(abs(diff(data_in.skt(l1:l2)))) <= 0.0001 %identify flat lines 
+        if max(abs(diff(temperature(l1:l2)))) <= 0.0001 %identify flat lines 
                 temp_sqi(l1:l2) = 0.50*temp_sqi(l1:l2);
         end    
          if abs(temp_maxmin) > 4 %identify steep increases/decreases
@@ -112,15 +114,15 @@ function [ sc_sqi, temp_sqi] = signal_quality_index(skin_conductance, temperatur
     if mod(L, 2) ~=0 && L>50
         l1=L-W;
         l2=L;
-        dff= std(data_in.skt(l1:l2));
-        temp_min = min(data_in.skt(l1:l2));
-        temp_maxmin = max(data_in.skt(l1:l2))-min(data_in.skt(l1:l2));
+        dff= std(temperature(l1:l2));
+        temp_min = min(temperature(l1:l2));
+        temp_maxmin = max(temperature(l1:l2))-min(temperature(l1:l2));
         temp_sqi(l1:l2)=temp_sqi(l1:l2)*exp(-0.20*dff);
 
         if temp_min < 15
             temp_sqi(l1:l2) = 0.50*temp_sqi(l1:l2);
         end
-        if max(abs(diff(data_in.skt(l1:l2)))) < 0.0001
+        if max(abs(diff(temperature(l1:l2)))) < 0.0001
             temp_sqi(l1:l2) = 0.50*temp_sqi(l1:l2);
         end     
         if abs(temp_maxmin) > 4
