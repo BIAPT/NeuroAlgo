78eb792c677e9f168e004cbddf7e2e593c1e3089
Bl0101 <yacine.mahdid@live.com>
2019-11-17 12:22:00 -0500

    add a good amount of code to create the visualization we need for the weights

--- Python/_playground/testing_out_library.py
+++ Python/_playground/testing_out_library.py
@@ -7,42 +7,33 @@
 
 import numpy as np
 
-from visbrain.gui import Brain
+from visbrain.gui import Brain, Figure
 from visbrain.objects import SourceObj, ConnectObj
 from visbrain.io import download_file
 
-# Create an empty kwargs dictionnary :
-kwargs = {}
-
-# ____________________________ DATA ____________________________
-
 # Load the xyz coordinates and corresponding subject name :
 mat = np.load(download_file('xyz_sample.npz', astype='example_data'))
-xyz, subjects = mat['xyz'], mat['subjects']
+xyz = mat['xyz']
 
 # An npz is a archive version of npy which is simply a numpy array
-
-print(xyz)
-print(subjects)
-
-#exit()
-
 num_electrodes = xyz.shape[0]  # Number of electrodes
 
 # Now, create some random data between [-50,50]
-data = np.random.uniform(-50, 50, num_electrodes)
+data = np.random.uniform(-50, 50, num_electrodes) 
+# THIS IS THE NODE DATA (IT CAN BE SOMETHING LIKE ITS AVERAGE GLOBAL CONNECTIVITY)
+# This will control the radius of the ball
 
 """Create the source object :
 """
 s_obj = SourceObj('SourceObj1', xyz, data, color='crimson', alpha=.5,
-                  edge_width=2., radius_min=2., radius_max=10.)
+                  edge_width=2., radius_min=0., radius_max=20.)
 
 """
 To connect sources between them, we create a (N, N) array.
 This array should be either upper or lower triangular to avoid
 redondant connections.
 """
-connect = 1000 * np.random.rand(num_electrodes, num_electrodes)               # Random array of connections
+connect = 1000 * np.random.rand(num_electrodes, num_electrodes)
 connect[np.tril_indices_from(connect)] = 0  # Set to zero inferior triangle
 
 """
@@ -51,27 +42,41 @@ only certain either using a select array composed with ones and zeros, or by
 masking the connection matrix. We are giong to search vealues between umin and
 umax to limit the number of connections :
 """
-umin, umax = 30, 31
+small_weight_threshold = 1
+large_weight_threshold = 999
 
-# 1 - Using select (0: hide, 1: display):
-select = np.zeros_like(connect)
-select[(connect > umin) & (connect < umax)] = 1
+# Here we need two threshold where above large is the big weight
+# and small weight we below is the small weights
 
 # 2 - Using masking (True: hide, 1: display):
 connect = np.ma.masked_array(connect, mask=True)
-connect.mask[np.where((connect > umin) & (connect < umax))] = False
-
-print('1 and 2 equivalent :', np.array_equal(select, ~connect.mask + 0))
+# This will show (set to hide = False) only the large and small connection
+connect.mask[np.where((connect >= large_weight_threshold) | (connect <= small_weight_threshold))] = False
+#print('1 and 2 equivalent :', np.array_equal(select, ~connect.mask + 0))
 
 """Create the connectivity object :
 """
 c_obj = ConnectObj('ConnectObj1', xyz, connect, color_by='strength',
-                   dynamic=(.1, 1.), cmap='gnuplot', vmin=umin + .2,
-                   vmax=umax - .1, under='red', over='green',
-                   clim=(umin, umax), antialias=True)
+                   cmap='Greys', vmin=small_weight_threshold,
+                   vmax=large_weight_threshold, under='blue', over='red',
+                   antialias=True)
 
 """Finally, pass source and connectivity objects to Brain :
 """
 vb = Brain(source_obj=s_obj, connect_obj=c_obj)
 
-vb.show()
+vb.screenshot('main.png', canvas='main', print_size=(10, 20))
+
+# What is left to do here is to take different shot of the brain to create a figure of the overal connectivity
+# through time
+
+#vb.show()
+
+'''
+    Bottom line: We can't set the width of our edges to be bigger or smaller
+    We can modify the colormap thought.
+
+    we can use a combination of vmin,under, vmax,over and cmap to highlight
+    different connection stength
+
+'''
