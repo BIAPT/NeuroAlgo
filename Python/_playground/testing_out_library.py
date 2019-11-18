565b389ab0eae28b32131c52dd5b1b4a7de65d9e
Bl0101 <yacine.mahdid@live.com>
2019-11-17 19:11:51 -0500

    add some starter code for the visualization (still some work to do to make a video!)

--- Python/_playground/testing_out_library.py
+++ Python/_playground/testing_out_library.py
@@ -18,23 +18,26 @@ xyz = mat['xyz']
 # An npz is a archive version of npy which is simply a numpy array
 num_electrodes = xyz.shape[0]  # Number of electrodes
 
-# Now, create some random data between [-50,50]
-data = np.random.uniform(-50, 50, num_electrodes) 
 # THIS IS THE NODE DATA (IT CAN BE SOMETHING LIKE ITS AVERAGE GLOBAL CONNECTIVITY)
 # This will control the radius of the ball
-
-"""Create the source object :
-"""
-s_obj = SourceObj('SourceObj1', xyz, data, color='crimson', alpha=.5,
-                  edge_width=2., radius_min=0., radius_max=20.)
 
 """
 To connect sources between them, we create a (N, N) array.
 This array should be either upper or lower triangular to avoid
 redondant connections.
 """
-connect = 1000 * np.random.rand(num_electrodes, num_electrodes)
+# Get the connection matrix
+connect = 1 * np.random.rand(num_electrodes, num_electrodes)
+
+data = np.mean(connect, axis=1)
 connect[np.tril_indices_from(connect)] = 0  # Set to zero inferior triangle
+
+
+
+"""Create the source object :
+"""
+s_obj = SourceObj('SourceObj1', xyz, data, color='whitesmoke', alpha=.5,
+                  edge_width=2., radius_min=29.9, radius_max=30.)
 
 """
 Because all connections are not necessary interesting, it's possible to select
@@ -42,8 +45,8 @@ only certain either using a select array composed with ones and zeros, or by
 masking the connection matrix. We are giong to search vealues between umin and
 umax to limit the number of connections :
 """
-small_weight_threshold = 1
-large_weight_threshold = 999
+small_weight_threshold = 0.001
+large_weight_threshold = 0.999
 
 # Here we need two threshold where above large is the big weight
 # and small weight we below is the small weights
@@ -64,8 +67,19 @@ c_obj = ConnectObj('ConnectObj1', xyz, connect, color_by='strength',
 """Finally, pass source and connectivity objects to Brain :
 """
 vb = Brain(source_obj=s_obj, connect_obj=c_obj)
+vb.brain_control(alpha=0.1, hemisphere='both')
 
-vb.screenshot('main.png', canvas='main', print_size=(10, 20))
+rotation = 'axial_0'
+vb.rotate('axial_0') # top
+#vb.rotate('axial_1') #bottom
+#vb.rotate('sagittal_0') # left
+#vb.rotate('sagittal_1') # right
+#vb.rotate('coronal_0') # front
+#vb.rotate('coronal_1') # back
+
+# Save the brain
+vb.screenshot('main2.png', canvas='main', print_size=(10, 20),dpi=300, autocrop=True)
+
 
 # What is left to do here is to take different shot of the brain to create a figure of the overal connectivity
 # through time
@@ -78,5 +92,33 @@ vb.screenshot('main.png', canvas='main', print_size=(10, 20))
 
     we can use a combination of vmin,under, vmax,over and cmap to highlight
     different connection stength
+
+    to rotate the view in the image we need to do:
+        vb.rotate(one of the below)
+
+        Top view : ‘axial_0’, ‘top’
+
+        Bottom view : ‘axial_1’, ‘bottom’
+
+        Left : ‘sagittal_0’, ‘left’
+
+        Right : ‘sagittal_1’, ‘right’
+
+        Front : ‘coronal_0’, ‘front’
+
+        Back : ‘coronal_1’, ‘back’
+
+        Side front-left : ‘side-fl’
+
+        Side front-right : ‘side-fr’
+
+        Side back-left : ‘side-bl’
+
+        Side back-right : ‘side-br’
+
+    if we want only to see one of the side or both we need to do
+    vb.brain_control(hemisphere='left' or 'right' or 'both)
+
+
 
 '''
