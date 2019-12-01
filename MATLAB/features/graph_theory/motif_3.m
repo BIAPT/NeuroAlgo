f45dd743618fd84341b446f0d1e7cf4ffc7fb15e
Bl0101 <yacine.mahdid@live.com>
2019-12-01 16:02:47 -0500

    add normalization and plot of motifs

--- MATLAB/features/graph_theory/motif_3.m
+++ MATLAB/features/graph_theory/motif_3.m
@@ -1,4 +1,4 @@
-function [intensity, coherence, frequency, norm_intensity, norm_coherence, norm_frequency] = motif_3(network,number_rand_network, bin_swaps, weight_frequency)
+function [intensity, coherence, frequency] = motif_3(network,number_rand_network, bin_swaps, weight_frequency)
 %MOTIF_3    
     
     %% 1) Calculate the motif for our network of interest.
@@ -31,8 +31,19 @@ function [intensity, coherence, frequency, norm_intensity, norm_coherence, norm_
     cat_rand_coherence = squeeze(cat_rand_coherence)';    
     cat_rand_frequency = squeeze(cat_rand_frequency)';
 
-    norm_intensity = (mean(intensity') - mean(cat_rand_intensity)) ./ std(cat_rand_intensity);    
-    norm_coherence = (mean(coherence') - mean(cat_rand_coherence)) ./ std(cat_rand_coherence);
-    norm_frequency = (mean(frequency') - mean(cat_rand_frequency)) ./ std(cat_rand_frequency);
+    z_intensity = (mean(intensity') - mean(cat_rand_intensity)) ./ std(cat_rand_intensity);    
+    z_coherence = (mean(coherence') - mean(cat_rand_coherence)) ./ std(cat_rand_coherence);
+    z_frequency = (mean(frequency') - mean(cat_rand_frequency)) ./ std(cat_rand_frequency);
     
+    
+    % Here we set to 0 whatever is not significant at p < 0.05 (this should
+    % be a parameter though)
+    for i=1:13
+        % Need to set the count for these motifs to 0 
+        if(z_frequency(i) < 1.96) 
+           for j=1:length(frequency)
+              frequency(i,j) = 0;
+           end
+        end
+    end
 end
