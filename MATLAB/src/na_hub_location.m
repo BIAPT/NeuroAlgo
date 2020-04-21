d5d8ee6f6170c0b1859ea4798d9fd7ee1fc7c391
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-04-21 19:33:22 -0400

    add more helpful help message

--- MATLAB/src/na_hub_location.m
+++ MATLAB/src/na_hub_location.m
@@ -1,6 +1,41 @@
 function [result] = na_hub_location(recording, frequency_band, window_size, step_size, number_surrogate, p_value ,threshold)
-%NA_HUB_LOCATION Summary of this function goes here
-%   Detailed explanation goes here
+%NA_HUB_LOCATION will calculate hub locations
+%(as defined by betweeness-centrality and degree) for the whole recording 
+%of EEG data and store it inside a result data structure. 
+%
+%   this function will take a EEG data bundled inside an recording instance
+%   and will calculate wPLI on segment of the data. It will then binarize
+%   these matrices of connectivity before calculating the binary hub
+%   location. It will output both the X-coordinate position of the hub
+%   along the anterior-posterior axis and the weights calculated to defined
+%   a hub. The hub here is defined as the maximum values in the map formed
+%   by doing degree+ betweness_centrality.
+%   
+%   input:
+%   recording: a EEG data bundled into a recording instance
+%   frequency_band: the frequency band at which to filter the data
+%   window_size: the window size onto which to cut the segment of data
+%   step_size: the step size at which to jump from one window to another if
+%   its the same as the window size it will be a jumpy windowing.
+%   number_surrogate: the number of surrogate wPLI matrices to make the
+%   null distribution for the corrected wPLI calculation
+%   p_value: p value at which to say that a connection is significant
+%   threshold: from 0 to 1 it's the amount of top connection we want to
+%   keep in the wPLI->binary_wpli.
+%
+%   output:
+%   result: a datastructure containing all the parameters information,
+%   metadata as well as the output of the hub location.
+%
+%   example usage:
+%   recording = load_set('name_of_data.set',path_to_data);
+%   frequency_band = [7 13]; 
+%   window_size = 10; 
+%   number_surrogate = 10; 
+%   p_value = 0.05; 
+%   threshold = 0.10;
+%   step_size = 10;
+%   result_hl = na_hub_location(recording, frequency_band, window_size, step_size, number_surrogate, p_value, threshold);
 
     %% Getting the configuration
     configuration = get_configuration();
