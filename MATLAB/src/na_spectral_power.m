cbb6f99c46f5646ef714cb357e29e3a6bcb02e3e
dnadin <danielle.nadin@mail.mcgill.ca>
2020-04-29 14:48:51 -0400

    fixed timestamp and removed redundant input variable
    timestamp was not updating correctly when you use a sliding window (was giving same timestamp for each window)
    
    the variable spectrum_window_size was never being used (same thing as window_size), so I remvoed it

--- MATLAB/src/na_spectral_power.m
+++ MATLAB/src/na_spectral_power.m
@@ -1,4 +1,4 @@
-function [result] = na_spectral_power(recording, window_size, time_bandwidth_product,number_tapers,spectrum_window_size, bandpass, step_size)
+function [result] = na_spectral_power(recording, window_size, time_bandwidth_product,number_tapers, bandpass, step_size)
     %NA_SPECTRAL_POWER NeuroAlgo implementation of spr that works with Recording
  
     configuration = get_configuration();
@@ -8,7 +8,6 @@ function [result] = na_spectral_power(recording, window_size, time_bandwidth_pro
     result.parameters.window_size = window_size;
     result.parameters.time_bandwidth_product = time_bandwidth_product;
     result.parameters.number_tapers = number_tapers;
-    result.parameters.spectrum_window_size = spectrum_window_size;
     result.parameters.step_size = step_size;
     result.parameters.bandpass = bandpass;
     
@@ -26,7 +25,7 @@ function [result] = na_spectral_power(recording, window_size, time_bandwidth_pro
        print_message(strcat("Spectral Power at window: ",string(i)," of ", string(number_window)),configuration.is_verbose); 
        [recording, segment_data] = recording.get_next_window();
        
-       [avg_spectrum,spectrum,timestamp,frequency,peak_frequency] = spectral_power(segment_data,sampling_rate, bandpass, time_bandwidth_product,number_tapers,spectrum_window_size,step_size); 
+       [avg_spectrum,spectrum,timestamp,frequency,peak_frequency] = spectral_power(segment_data,sampling_rate, bandpass, time_bandwidth_product,number_tapers,window_size,step_size); 
        
        result.data.avg_spectrums(i) = avg_spectrum; 
        if (i == 1)
@@ -36,6 +35,7 @@ function [result] = na_spectral_power(recording, window_size, time_bandwidth_pro
           result.data.peak_frequency = peak_frequency;
        else
            result.data.spectrums = cat(3,result.data.spectrums,spectrum);
+           result.data.timestamps = cat(2,result.data.timestamps,timestamp+step_size*(i-1));
            result.data.peak_frequency = cat(2,result.data.peak_frequency,peak_frequency);
        end
        %result.data.spectrums = [result.data.spectrums,spectrum]; % TODO: find a way to initialize this properly
